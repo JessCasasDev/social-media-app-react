a0f47b2cea62490dd95904cb27847b0eb0cad9e4
Jess <jlcasasg@unal.edu.co>
2020-03-12 15:05:36 -0500

    feat: navbar-routing: Add navbar, set the route for login, signup, home

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Link from 'react-router-dom/Link';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <AppBar>
+                <Toolbar className="nav-container">
+                    <Button color="inherit" to="/home" component={Link}>
+                        Home
+                    </Button>
+                    <Button color="inherit" to="/login" component={Link}>
+                        Login
+                    </Button>
+
+                    <Button color="inherit" to="/signup" component={Link}>
+                        Signup
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        )
+    }
+
+}
+
+export default NavBar;
