da7d8ee5f438e9efc75bad33b115e11f217e278e
Jess <jlcasasg@unal.edu.co>
2020-03-12 17:43:28 -0500

    feat: Scream card: Show scream content  in card on home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+import Scream from '../components/Scream';
 
 class home extends Component {
+    state = {
+        screams: null
+    }
+    componentDidMount() {
+        axios.get('screams')
+            .then(result => {
+                this.setState({
+                    screams: result.data
+                })
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
     render() {
-        return <h1>
-            Home
-        </h1>
+        let recentScreamsMarkup =
+            this.state.screams
+                ? (this.state.screams.map((scream) => <div key={scream.screamId}><Scream scream={scream} /></div>))
+                : <p>Loading...</p>
+        return (
+            <Grid container spacing={10}>
+                <Grid item sm={8} xs={12}>
+                    {recentScreamsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>sfsdfdsf</p>safasf
+                </Grid>
+            </Grid>
+        )
     }
 }
 
